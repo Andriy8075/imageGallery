46032d5d6cad0423d17168b499caef2e0f5dfce7
andriy8075 <andriy8075@gmail.com>
2024-12-29 13:55:15 +0200

    split js code in many files

--- resources/js/imageDisplaying.js
+++ resources/js/imageDisplaying.js
@@ -1,6 +1,15 @@
+import {
+    placeImages,
+    loadImagesIfNeeded,
+    loadMoreImages,
+} from './imageUtils.js';
+
+import {
+    heights,
+} from './state.js';
+
 const windowWidth = window.innerWidth
 const countOfCols = Math.ceil(windowWidth/initialData.imageLoadingConfig.image_max_width)
-const heights = [];
 for (let i = 0; i<countOfCols; i++) {
     const colDiv = document.createElement('div');
     colDiv.classList.add('col');
@@ -11,108 +20,12 @@ for (let i = 0; i<countOfCols; i++) {
     heights[i] = 0;
 }
 
-const addImage = (link) => {
-    const cols = document.getElementsByClassName('col');
-    const imageDiv = document.createElement('div');
-    imageDiv.style.marginBottom = '16px'
-    const imgElement = document.createElement('img');
-
-    imgElement.src = link.url;
-    imgElement.classList.add('image-item');
-    imageDiv.appendChild(imgElement);
-
-    const resizeImage = (img) => {
-        const maxWidth = initialData.imageLoadingConfig.image_max_width;
-        const aspectRatio = img.naturalWidth / img.naturalHeight;
-
-        img.width = maxWidth;
-        img.height = Math.round(maxWidth / aspectRatio);
-    };
-
-    imgElement.onload = () => {
-        resizeImage(imgElement);
-
-        const shortestColIndex = findShortestColumn();
-        cols[shortestColIndex].appendChild(imageDiv);
-
-        const marginBottom = parseFloat(imageDiv.style.marginBottom)
-        heights[shortestColIndex] += (imgElement.height+marginBottom);
-    };
-
-    imgElement.onerror = () => {
-        console.error('Image failed to load:', imgElement.src);
-    };
-};
-
-const findShortestColumn = () => {
-    let shortestHeight = -1;
-    let shortestIndex = 0;
-
-    for (let i = 0; i < heights.length; i++) {
-        const height = heights[i];
-        if (shortestHeight < 0 || height < shortestHeight) {
-            shortestHeight = height;
-            shortestIndex = i;
-        }
-    }
-    return shortestIndex;
-};
-
-async function placeImages(images) {
-    const links = images;
-    const imageLoadPromises = [];
-
-    for (const link of links) {
-        const loadPromise = new Promise((resolve) => {
-            const imgElement = document.createElement('img');
-            imgElement.src = link.url;
-            imgElement.onload = resolve;
-            imgElement.onerror = resolve;
-        });
-        imageLoadPromises.push(loadPromise);
-
-        addImage(link);
-    }
-
-    await Promise.all(imageLoadPromises);
-}
-
-async function loadImagesIfNeeded() {
-    while (true) {
-        if ($(document).height() <= $(window).height()) {
-            await loadMoreImages();
-        } else {
-            break;
-        }
-    }
-}
-
-async function loadMoreImages() {
-    const loadingLabel = $('#loading');
-    loadingLabel.show();
-
-    try {
-        const response = await $.ajax({
-            url: initialData.loadMoreUrl,
-            type: 'GET',
-        });
-
-        if (response.images) {
-            await placeImages(response.images)
-        }
-    } catch (error) {
-        console.error('Could not load more images.', error);
-    } finally {
-        loadingLabel.hide();
-    }
-}
-
 
 let hasMorePages = initialData.images.hasMorePages;
 $(document).ready(function () {
     (async () => {
         const images = initialData.images.images;
-        await placeImages(images);
+        await placeImages(images, heights);
         await loadImagesIfNeeded();
 
         let isLoading = false;
---
+++ resources/js/imageUtils.js
@@ -0,0 +1,99 @@
+import {
+    heights,
+} from './state.js';
+
+const findShortestColumn = () => {
+    let shortestHeight = -1;
+    let shortestIndex = 0;
+
+    for (let i = 0; i < heights.length; i++) {
+        const height = heights[i];
+        if (shortestHeight < 0 || height < shortestHeight) {
+            shortestHeight = height;
+            shortestIndex = i;
+        }
+    }
+    return shortestIndex;
+};
+
+const resizeImage = (img) => {
+    const maxWidth = initialData.imageLoadingConfig.image_max_width;
+    const aspectRatio = img.naturalWidth / img.naturalHeight;
+
+    img.width = maxWidth;
+    img.height = Math.round(maxWidth / aspectRatio);
+};
+
+const addImage = (link) => {
+    const cols = document.getElementsByClassName('col');
+    const imageDiv = document.createElement('div');
+    imageDiv.style.marginBottom = '16px'
+    const imgElement = document.createElement('img');
+
+    imgElement.src = link.url;
+    imgElement.classList.add('image-item');
+    imageDiv.appendChild(imgElement);
+
+    imgElement.onload = () => {
+        resizeImage(imgElement);
+
+        const shortestColIndex = findShortestColumn();
+        cols[shortestColIndex].appendChild(imageDiv);
+
+        const marginBottom = parseFloat(imageDiv.style.marginBottom)
+        heights[shortestColIndex] += (imgElement.height+marginBottom);
+    };
+
+    imgElement.onerror = () => {
+        console.error('Image failed to load:', imgElement.src);
+    };
+};
+
+export const placeImages = async (images) => {
+    const links = images;
+    const imageLoadPromises = [];
+
+    for (const link of links) {
+        const loadPromise = new Promise((resolve) => {
+            const imgElement = document.createElement('img');
+            imgElement.src = link.url;
+            imgElement.onload = resolve;
+            imgElement.onerror = resolve;
+        });
+        imageLoadPromises.push(loadPromise);
+
+        addImage(link);
+    }
+
+    await Promise.all(imageLoadPromises);
+}
+
+export const loadMoreImages = async () => {
+    const loadingLabel = $('#loading');
+    loadingLabel.show();
+
+    try {
+        const response = await $.ajax({
+            url: initialData.loadMoreUrl,
+            type: 'GET',
+        });
+
+        if (response.images) {
+            await placeImages(response.images)
+        }
+    } catch (error) {
+        console.error('Could not load more images.', error);
+    } finally {
+        loadingLabel.hide();
+    }
+}
+
+export const loadImagesIfNeeded = async () =>{
+    while (true) {
+        if ($(document).height() <= $(window).height()) {
+            await loadMoreImages();
+        } else {
+            break;
+        }
+    }
+}

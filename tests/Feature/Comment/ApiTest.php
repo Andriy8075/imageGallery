f2cfff57aaed09f8697e3e6fc749572c80d03285
andriy8075 <andriy8075@gmail.com>
2025-05-19 23:07:21 +0300

    correct tests

--- tests/Feature/Comment/ApiTest.php
+++ tests/Feature/Comment/ApiTest.php
@@ -2,19 +2,18 @@
 
 use App\Models\User;
 use App\Models\Image;
-use Illuminate\Support\Str;
 
 test('authenticated can comment', function () {
     $user1 = User::factory()->create();
     $user2 = User::factory()->create();
-    Image::factory()->create(['user_id' => $user1->id]);
+    $image = Image::factory()->create(['user_id' => $user1->id]);
 
     //can comment own image
-    $response1 = $this->actingAs($user1)->post("/comments/1", ['text' => 'bebra1']);
+    $response1 = $this->actingAs($user1)->post("/comments/{$image->id}", ['text' => 'bebra1']);
     $response1->assertStatus(200);
 
     //can comment another user's image
-    $response2 = $this->actingAs($user2)->post("/comments/1", ['text' => 'bebra2']);
+    $response2 = $this->actingAs($user2)->post("/comments/{$image->id}", ['text' => 'bebra2']);
     $response2->assertStatus(200);
 
     $this->assertDatabaseHas('comments', ['text' => 'bebra1']);
@@ -23,9 +22,9 @@ test('authenticated can comment', function () {
 
 test('unauthenticated can not comment', function () {
     $user = User::factory()->create();
-    Image::factory()->create(['user_id' => $user->id]);
+    $image = Image::factory()->create(['user_id' => $user->id]);
 
     //can comment own image
-    $this->post("/comments/1", ['text' => 'bebra1']);
+    $this->post("/comments/{$image->id}", ['text' => 'bebra1']);
     $this->assertDatabaseCount('comments', 0);
 });
--- tests/Feature/Comment/largeTextTest.php
+++ tests/Feature/Comment/largeTextTest.php
@@ -6,23 +6,25 @@ use Illuminate\Support\Str;
 
 test('too long', function() {
     $user = User::factory()->create();
-    Image::factory()->create(['user_id' => $user->id]);
+    $image = Image::factory()->create(['user_id' => $user->id]);
 
     $commentLength = config('comments.max_length') + 1;
     $text = Str::random($commentLength);
-    $this->actingAs($user)->post("/comments/1", [
+    $this->actingAs($user)->post("/comments/{$image->id}", [
         'text' => $text
     ]);
     $this->assertDatabaseCount('comments', 0);
 
     $acceptableSizeText = substr($text, 0, strlen($text) - 1);
-    $this->actingAs($user)->post("/comments/1", [
+    $response = $this->actingAs($user)->post("/comments/{$image->id}", [
         'text' => $acceptableSizeText
     ]);
+
+    $response->assertStatus(200); // Add this check
     $this->assertDatabaseCount('comments', 1);
     $this->assertDatabaseHas('comments', [
         'text' => $acceptableSizeText,
         'user_id' => $user->id,
-        'image_id' => $user->image_id,
+        'image_id' => $image->id, // Use the image reference
     ]);
 });

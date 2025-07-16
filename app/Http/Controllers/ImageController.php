95a004106872f7711e1c5a6f6cd151cfd5b1123e
andriy8075 <andriy8075@gmail.com>
2025-07-16 20:56:14 +0300

    delete unnecessary import

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -6,7 +6,6 @@ use Illuminate\Http\Request;
 use App\Models\Image;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Storage;
-use Illuminate\Support\Facades\Log;
 class ImageController extends Controller
 {
     protected $queries = [];

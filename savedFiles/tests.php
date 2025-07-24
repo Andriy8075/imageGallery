a9e3ac2b003d476be5a1e5b73f778b075576ac68
andriy8075 <andriy8075@gmail.com>
2025-07-24 11:11:20 +0300

    delete savedFiles from git

--- savedFiles/tests.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    "images" => [
-        "url" => function($width, $height) {
-            return "https://picsum.photos/{$width}/{$height}?random=" . rand(1, 100000);
-        },
-    ],
-];

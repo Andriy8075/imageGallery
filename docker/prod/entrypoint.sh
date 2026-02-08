5e32d47554b07970c4ad112d673c2594ee0aaf7e
andriy8075 <andriy8075@gmail.com>
2026-02-08 13:14:53 +0000

    force migrate for production

--- docker/prod/entrypoint.sh
+++ docker/prod/entrypoint.sh
@@ -2,5 +2,5 @@ cd /var/www || exit 1
 if [ ! -f .env ]; then cp docker/prod/.env.example .env; fi
 php artisan key:generate
 php artisan storage:link
-php artisan migrate
+php artisan migrate --force
 php artisan serve --host=0.0.0.0 --port=8000

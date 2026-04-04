a1013850ea65b8c1b5247db4070d3d890c637d1e
andriy8075 <andriy8075@gmail.com>
2026-04-04 18:12:18 +0100

    finish entrypoint

--- docker/lab3/entrypoint.sh
+++ docker/lab3/entrypoint.sh
@@ -3,3 +3,4 @@ if [ ! -f .env ]; then cp docker/lab3/.env.example .env; fi
 php artisan key:generate
 php artisan storage:link
 php artisan migrate --force
+php artisan serve --host=0.0.0.0 --port=8000

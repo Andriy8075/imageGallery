68fa487149b1f2c787b128f74177f6ebdff9a5bd
andriy8075 <andriy8075@gmail.com>
2026-04-07 11:00:24 +0100

    wait untill db is ready before running migrations

--- docker/prod/entrypoint.sh
+++ docker/prod/entrypoint.sh
@@ -1,6 +1,14 @@
 cd /var/www || exit 1
-if [ ! -f .env ]; then cp docker/prod/.env.example .env; fi
 php artisan key:generate
 php artisan storage:link
-php artisan migrate --force
+
+echo "Waiting for database..."
+until php artisan db:monitor; do
+  echo "Database is unavailable - sleeping"
+  sleep 2
+done
+
+echo "Running migrations..."
+php artisan migrate || exit 1
+
 php artisan serve --host=0.0.0.0 --port=8000
